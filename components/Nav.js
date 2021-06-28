6b6cbca2d503dbf4f57ae708563c310b3eaa846d
John <exarhouleasjohn@gmail.com>
2021-06-28 15:51:51 -0400

    Final design

--- components/Nav.js
+++ components/Nav.js
@@ -9,20 +9,28 @@ const Nav = () => {
 
     const showDropdown = () => {
         changeDropdownVisibility('')
+        dropdown.current.style.opacity = '1'
         dropdown.current.style.right = '-4rem'
         dropdown.current.style.top = '12rem'
         dropdown.current.style.height = '16rem'
         dropdown.current.style.width = '10rem'
-        dropdown.current.style.color = 'black'
+        setTimeout(() => {
+            dropdown.current.style.color = 'black'
+            dropdown.current.style.fontSize = '18px'
+        }, 140)
     }
     
     const hideDropdown = () => {
         changeDropdownVisibility('hidden')
+        dropdown.current.style.opacity = '0'
         dropdown.current.style.right = '5.6rem'
+        // dropdown.current.style.right = '-4rem'
         dropdown.current.style.top = '3.5rem'
         dropdown.current.style.height = '0rem'
         dropdown.current.style.width = '0rem'
+        // dropdown.current.style.width = '10rem'
         dropdown.current.style.color = 'white'
+        dropdown.current.style.fontSize = '0px'
     }
 
     return (
@@ -36,7 +44,13 @@ const Nav = () => {
                 <li><Link href='/accounts'>Accounts</Link></li>
                 <li className={navStyles.user} onMouseEnter={showDropdown} onMouseLeave={hideDropdown} >
                     <Link href='/user'><img src='https://i.ibb.co/LZdfPsd/account.png' alt='user'></img></Link>
-                    <div className={navStyles.dropdown} ref={dropdown} style={{visibility: dropdownVisibility}} ></div>
+                    <div className={navStyles.connector}></div>
+                    <div className={navStyles.dropdown} ref={dropdown} style={{visibility: dropdownVisibility}} >
+                        <ul>
+                            <li>Login</li>
+                            <li>Register</li>
+                        </ul>
+                    </div>
                 </li>
             </ul>
         </div>
